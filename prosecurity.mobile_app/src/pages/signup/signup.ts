e495711602b78d88d3f9bd344499697456e836ce
madina1467 <madinaa.1467@gmail.com>
2019-04-19 14:52:43 +0600

    mobile: add validators for registration enter column

--- prosecurity.mobile_app/src/pages/signup/signup.ts
+++ prosecurity.mobile_app/src/pages/signup/signup.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 /**
  * Generated class for the SignupPage page.
@@ -13,13 +14,77 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   selector: 'page-signup',
   templateUrl: 'signup.html',
 })
-export class SignupPage {
+export class SignupPage implements OnInit  {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  userForm: FormGroup;
+  formErrors = {
+    'email': '',
+    'password': ''
+  };
+  validationMessages = {
+    'email': {
+      'required': 'Please enter your email',
+      'email': 'please enter your valid email'
+    },
+    'password': {
+      'required': 'please enter your password',
+      'pattern': 'The password must contain numbers and letters',
+      'minlength': 'Please enter more than 4 characters',
+      'maxlength': 'Please enter less than 25 characters',
+    }
+  };
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+              private fb: FormBuilder) {
+  }
+  ngOnInit() {
+    this.buildForm();
+  }
+  buildForm() {
+    this.userForm = this.fb.group({
+      'email': ['', [
+        Validators.required,
+        Validators.email
+      ]
+      ],
+      'password': ['', [
+        Validators.pattern('^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$'),
+        Validators.minLength(6),
+        Validators.maxLength(25)
+      ]
+      ],
+    });
+
+    this.userForm.valueChanges.subscribe(data => this.onValueChanged(data));
+    this.onValueChanged();
+  }
+  onValueChanged(data?: any) {
+    if (!this.userForm) {
+      return;
+    }
+    const form = this.userForm;
+    for (const field in this.formErrors) {
+      if (Object.prototype.hasOwnProperty.call(this.formErrors, field)) {
+        this.formErrors[field] = '';
+        const control = form.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+            if (Object.prototype.hasOwnProperty.call(control.errors, key)) {
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SignupPage');
+  }
+
+  register(){
+    console.log(this.userForm);
   }
 
   goToSignin() {
