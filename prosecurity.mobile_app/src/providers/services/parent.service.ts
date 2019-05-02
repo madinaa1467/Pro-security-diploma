614efcd4b65abaf7e8a5813b291c58512c8b49b6
madina1467 <madinaa.1467@gmail.com>
2019-05-02 07:51:22 +0600

    mobile-server: fix error formControl

--- prosecurity.mobile_app/src/providers/services/parent.service.ts
+++ prosecurity.mobile_app/src/providers/services/parent.service.ts
@@ -1,20 +1,18 @@
 import {Injectable} from '@angular/core';
-import {Api, AppLoader, Auth} from "../index";
+import {Api} from "../index";
 import {EventFilter} from "../../model/EventFilter";
 import {ToSave} from "../../model/ToSave";
-import {AccountInfo} from "../../model/auth/account-info";
-import {Subject} from "rxjs/Subject";
 
 @Injectable()
 export class ParentService {
 
-  constructor(private http: Api, private appLoader: AppLoader, private auth: Auth) {
+  constructor(private http: Api) {
   }
   public filter: EventFilter = new EventFilter();
 
   loadParentInfo():Promise<ToSave> {
     return this.http.get("parent/getInfo")
       .toPromise()
-      .then(resp => ToSave.create(resp)).catch(err=>console.error(err));
+      .then(resp => ToSave.create(resp));
   }
 }
