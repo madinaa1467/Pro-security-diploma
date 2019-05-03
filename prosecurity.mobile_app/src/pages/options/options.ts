f92b21009fc9a99c1f05f32498f1e1a1b24e381f
madina1467 <madinaa.1467@gmail.com>
2019-05-03 18:03:17 +0600

    mobile: change settings page

--- prosecurity.mobile_app/src/pages/options/options.ts
+++ prosecurity.mobile_app/src/pages/options/options.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {IonicPage, ModalController, NavController, NavParams} from 'ionic-angular';
+import {EditProfile} from "../edit-profile/edit-profile";
 
 /**
  * Generated class for the Options page.
@@ -14,11 +15,17 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class Options {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+              public modalCtrl: ModalController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad Options');
   }
 
+  goEditProfile() {
+    // Open it as a modal page
+    let modal = this.modalCtrl.create(EditProfile);
+    modal.present();
+  }
 }
