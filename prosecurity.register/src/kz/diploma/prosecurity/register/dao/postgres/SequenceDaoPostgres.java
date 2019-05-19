b4df9ec9b14a71a362413ebeab422a81095b7a99
madina1467 <madinaa.1467@gmail.com>
2019-05-19 10:36:27 +0600

    server: create sequence Dao to generate id

---
+++ prosecurity.register/src/kz/diploma/prosecurity/register/dao/postgres/SequenceDaoPostgres.java
@@ -0,0 +1,7 @@
+package kz.diploma.prosecurity.register.dao.postgres;
+
+import kz.diploma.prosecurity.register.dao.SequenceDao;
+import kz.greetgo.depinject.core.Bean;
+
+@Bean
+public interface SequenceDaoPostgres extends SequenceDao {}
