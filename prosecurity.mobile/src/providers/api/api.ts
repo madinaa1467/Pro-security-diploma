f54fc498494752ec25002e963f1900a0455f3cf1
madina1467 <madinaa.1467@gmail.com>
2019-05-20 07:33:53 +0600

    server: fixed cross-origin to connect mobile prosecurity app

--- prosecurity.mobile/src/providers/api/api.ts
+++ prosecurity.mobile/src/providers/api/api.ts
@@ -5,7 +5,7 @@ import {isUndefined} from "util";
 @Injectable()
 export class Api {
 
-  //private _url: string = 'http://192.168.11.181:1313/prosecurity/api';
+  //private _url: string = 'http://192.168.11.181:8080/prosecurity/api';
 
   private _url: string = 'https://diplomaproject.info/prosecurity/api';//'http://192.168.26.193:1313/prosecurity/api';
 
