195edd1f980715c017f9e6bf4ae02b5197d71070
msultanova <msultanova@gitlab.greetgo>
2019-05-25 12:41:37 +0600

    mobile: profile page- change css

--- prosecurity.mobile/src/pages/profile/profile.ts
+++ prosecurity.mobile/src/pages/profile/profile.ts
@@ -1,11 +1,7 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {IonicPage, ModalController, NavController, NavParams} from 'ionic-angular';
 
-import {ModalPost} from '../modal-post/modal-post';
-import {EditProfile} from '../edit-profile/edit-profile';
 import {Options} from '../options/options';
-import {TaggedProfile} from '../tagged-profile/tagged-profile';
-import {SavedProfile} from '../saved-profile/saved-profile';
 import {ChildPofile} from "../child-pofile/child-pofile";
 import {ChildService} from "../../providers/services/child.service";
 import {Child} from "../../model/Child";
@@ -36,22 +32,8 @@ export class Profile implements OnInit, OnDestroy  {
     this.accountInfo$.unsubscribe();
   }
 
-  goEditProfile() {
-    // Open it as a modal page
-    let modal = this.modalCtrl.create('EditProfile');
-    modal.present();
-  }
-
   goOptions() {
     this.navCtrl.push(Options, {});
-  }
-
-  goTaggedProfile() {
-    this.navCtrl.push(TaggedProfile);
-  }
-
-  goSavedProfile() {
-    this.navCtrl.push(SavedProfile);
   }
 
   // Triggers when user pressed a post
@@ -66,20 +48,5 @@ export class Profile implements OnInit, OnDestroy  {
     modal.present();
   }
 
-  // Set post modal
-  presentModal(user_id: number, username: string, profile_img: string, post_img: string) {
-
-    //this.presentModal(user_id, username, profile_img, post_img);
-
-    let modal = this.modalCtrl.create(ModalPost,
-    { // Send data to modal
-      user_id: user_id,
-      username: username,
-      profile_img: profile_img,
-      post_img: post_img
-    }, // This data comes from API!
-    { showBackdrop: true, enableBackdropDismiss: true });
-    modal.present();
-  }
 
 }
