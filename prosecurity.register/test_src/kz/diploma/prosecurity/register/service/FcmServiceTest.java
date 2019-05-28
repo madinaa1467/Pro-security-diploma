9710b6085914ff11696d7e747c4cb94af9c946b8
madina1467 <madinaa.1467@gmail.com>
2019-05-28 22:12:26 +0600

    server: tested fcm

--- prosecurity.register/test_src/kz/diploma/prosecurity/register/service/FcmServiceTest.java
+++ prosecurity.register/test_src/kz/diploma/prosecurity/register/service/FcmServiceTest.java
@@ -5,12 +5,19 @@ import kz.diploma.prosecurity.register.test.util.ParentTestNg;
 import kz.greetgo.depinject.core.BeanGetter;
 import org.testng.annotations.Test;
 
+import java.util.Map;
+
 public class FcmServiceTest extends ParentTestNg {
 
   public BeanGetter<FcmService> service;
 
   @Test
   public void testSendToParent() throws Exception {
-    service.get().sendToParent("asd", Maps.newHashMap());
+    String fcmToken = "fKgQD2Iaj88:APA91bFO1X9FS0nI6Zy4QuKa8y0IQYveGSIPTAKxmxgvCxl-XrBI5YrPHN1bSTyZ8Ge4FdMbPvRAG-zhzQ14wf-lTOY0cZuJXss5VUHhHMOyUEefA8kSpP8ce37_vmOuhGhZhfqEyVh1";
+    Map<String,String> data = Maps.newHashMap();
+    data.put("id", String.valueOf(123));
+    data.put("text", String.valueOf(Math.random() * 1000) + " asdfd dsdfsdf dsf");
+
+    service.get().sendToParent(fcmToken, data);
   }
 }
